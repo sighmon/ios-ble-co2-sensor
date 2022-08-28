21f68dfbf7caf7d4682a76cce5dbe7f89fa1777a
sighmon <simon.loffler@gmail.com>
2022-08-28 18:49:05 +0930

    Add map to archive view

--- CO2 sensor/ArchiveDetailView.swift
+++ CO2 sensor/ArchiveDetailView.swift
@@ -6,34 +6,59 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ArchiveDetailView: View {
     @Environment(\.managedObjectContext) private var viewContext
     var measurement: Measurement
 
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 37.334_900,
+            longitude: -122.009_020
+        ),
+        latitudinalMeters: 750,
+        longitudinalMeters: 750
+    )
+
     var body: some View {
         VStack {
+            Map(coordinateRegion: $region)
+                .onAppear {
+                    setRegion(CLLocationCoordinate2D(
+                        latitude: measurement.latitude,
+                        longitude: measurement.longitude)
+                    )
+                }
+                .padding(.top, 0)
+                .ignoresSafeArea()
             Text("\(measurement.co2)")
-                .padding(.top, -60)
+                .padding(.top, 0)
             Text("ppm CO₂")
                 .font(.system(size: 30, weight: .light))
-                .padding(.bottom, 60)
+                .padding(.bottom, 30)
             Text("\(measurement.temperature, specifier: "%.1f") °C")
                 .font(.system(size: 30, weight: .light))
                 .padding(.bottom, 10)
             Text("\(measurement.humidity, specifier: "%.1f") %")
                 .font(.system(size: 30, weight: .light))
-                .padding(.bottom, 10)
-            Text("Latitude: \(measurement.latitude)")
-                .font(.system(size: 20, weight: .light))
-            Text("Longitutde: \(measurement.longitude)")
-                .font(.system(size: 20, weight: .light))
-                .padding(.bottom, 10)
+                .padding(.bottom, 30)
             Text("\(measurement.timestamp ?? Date(), formatter: dateFormatter)")
                 .font(.system(size: 20, weight: .light))
+                .padding(.bottom, 30)
         }
         .font(.system(size: 80, weight: .medium))
     }
+
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+            region = MKCoordinateRegion(
+                center: coordinate,
+                span: MKCoordinateSpan(
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005
+                )
+            )
+        }
 }
 
 private let dateFormatter: DateFormatter = {
